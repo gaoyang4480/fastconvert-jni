c57abfe47b6324a87717aa3b929b8a02c3ad37d3
gaoyang <641782191@qq.com>
2021-07-13 13:53:54 +0800

    modify test case

--- src/com/scofd/gentemplate/FastConverJNI.java
+++ src/com/scofd/gentemplate/FastConverJNI.java
@@ -7,6 +7,7 @@ import com.yh.scofd.agent.wrapper.model.TempInfo;
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -14,7 +15,7 @@ import java.util.concurrent.Executors;
 
 public class FastConverJNI {
 
-    static final double convertCount = 1;
+    static final double convertCount = 100;
 
     public native int initConverter(String key, String configData);
 
@@ -74,7 +75,7 @@ public class FastConverJNI {
         }
 
         String templateDir = args[1];
-
+        /*
         List<TempInfo> tempInfoList = new ArrayList<>();
         TempInfo tempInfo = new TempInfo();
         byte[] imageData = getFileByteArray(Paths.get(templateBaseDir, templateDir, "Img_4.jpg").toFile());
@@ -89,73 +90,73 @@ public class FastConverJNI {
         tempInfo.setPatternHeight(50);
         tempInfoList.add(tempInfo);
         byte[] formData = getFileByteArray(Paths.get(templateBaseDir, templateDir, "form.xml").toFile());
-        //long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
         byte[] outputTemplateOFDData = convertJni.convertImageToTemplateOFD(0, 0, tempInfoList, formData);
-        //System.out.println("转换耗时： " + (System.currentTimeMillis() - startTime) + "ms");
+        System.out.println("转换耗时： " + (System.currentTimeMillis() - startTime) + "ms");
         if (outputTemplateOFDData != null) {
             OutputStream resultOutStream = new FileOutputStream(Paths.get(templateBaseDir, templateDir, "t.ofd").toString());
             resultOutStream.write(outputTemplateOFDData);
             resultOutStream.close();
         }
+        */
 
 
+        byte[] templateOFDData = getFileByteArray(Paths.get(templateBaseDir, templateDir, "template.ofd").toFile());
+        byte[] inData = getFileByteArray(Paths.get(templateBaseDir, templateDir, "data.xml").toFile());
+        // 构造元数据.
+        List<MetaData> metaDataList = new ArrayList<>();
+        MetaData metaData = new MetaData();
+        metaData.setName("test");
+        metaData.setValue("test value");
+        //metaDataList.add(metaData);
+        // 构造附件数据.
+        List<Attachments> attachmentDataList = new ArrayList<>();
+        Attachments attachments = new Attachments();
+        attachments.setName("original_invoice");
+        attachments.setFormat("xml");
+        attachments.setCreationDate("2020-07-28T12:38:11");
+        attachments.setModDate("2020-07-28T12:38:11");
+        attachments.setSize(2.415);
+        attachments.setVisible(true);
+        attachments.setFile("test".getBytes());
+        //attachmentDataList.add(attachments);
 
-//        byte[] templateOFDData = getFileByteArray(Paths.get(templateBaseDir, templateDir, "template.ofd").toFile());
-//        byte[] inData = getFileByteArray(Paths.get(templateBaseDir, templateDir, "data.xml").toFile());
-//        // 构造元数据.
-//        List<MetaData> metaDataList = new ArrayList<>();
-//        MetaData metaData = new MetaData();
-//        metaData.setName("test");
-//        metaData.setValue("test value");
-//        //metaDataList.add(metaData);
-//        // 构造附件数据.
-//        List<Attachments> attachmentDataList = new ArrayList<>();
-//        Attachments attachments = new Attachments();
-//        attachments.setName("original_invoice");
-//        attachments.setFormat("xml");
-//        attachments.setCreationDate("2020-07-28T12:38:11");
-//        attachments.setModDate("2020-07-28T12:38:11");
-//        attachments.setSize(2.415);
-//        attachments.setVisible(true);
-//        attachments.setFile("test".getBytes());
-//        //attachmentDataList.add(attachments);
-//
-//        List<Callable<Integer>> list = null;
-//        ExecutorService cachedThreadPool = Executors.newFixedThreadPool(40);
-//        long cc = System.currentTimeMillis();
-//        try {
-//            list = getCallableList(convertJni, templateOFDData, inData, metaDataList, attachmentDataList);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        try {
-//            Long beginTime = System.currentTimeMillis();
-//            cachedThreadPool.invokeAll(list);
-//            long endTime = System.currentTimeMillis();
-//            long coustTime = endTime - beginTime;
-//            System.out.println("**************************************************耗时:" + coustTime);
-//            System.out.println("总耗时：" + (endTime - cc) + "毫秒");
-//            System.out.println("平均每秒转换数：" + convertCount / ((endTime - cc) / 1000.0) + "");
-//            System.out.println("平均：" + (endTime - cc) / convertCount);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        List<Callable<Integer>> list = null;
+        ExecutorService cachedThreadPool = Executors.newFixedThreadPool(8);
+        long cc = System.currentTimeMillis();
+        try {
+            list = getCallableList(convertJni, templateOFDData, inData, metaDataList, attachmentDataList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            Long beginTime = System.currentTimeMillis();
+            cachedThreadPool.invokeAll(list);
+            long endTime = System.currentTimeMillis();
+            long coustTime = endTime - beginTime;
+            System.out.println("**************************************************耗时:" + coustTime);
+            System.out.println("总耗时：" + (endTime - cc) + "毫秒");
+            System.out.println("平均每秒转换数：" + convertCount / ((endTime - cc) / 1000.0) + "");
+            System.out.println("平均：" + (endTime - cc) / convertCount);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
 //        long startTime = System.currentTimeMillis();
 //        for (int i = 0; i < 1; i++) {
 //            byte[] resultData = convertJni.convert("001", templateOFDData, inData, metaDataList, attachmentDataList, 10);
-//            if (resultData != null) {
-//                OutputStream resultOutStream = new FileOutputStream(Paths.get(templateBaseDir, templateDir, "template_output.ofd").toString());
-//                resultOutStream.write(resultData);
-//                resultOutStream.close();
-//                System.out.println("write result ofd successfully");
-//            }
+////            if (resultData != null) {
+////                OutputStream resultOutStream = new FileOutputStream(Paths.get(templateBaseDir, templateDir, "template_output.ofd").toString());
+////                resultOutStream.write(resultData);
+////                resultOutStream.close();
+////                System.out.println("write result ofd successfully");
+////            }
 //        }
 //        System.out.println("转换耗时： " + (System.currentTimeMillis() - startTime) + "ms");
-//
-//        convertJni.finalizeConverter();
-//        System.out.println("convert end");
+
+        //convertJni.finalizeConverter();
+        System.out.println("convert end");
     }
 
     private static List<Callable<Integer>> getCallableList(FastConverJNI convertJni, byte[] templateOFDData, byte[] inData, List<MetaData> metaDataList, List<Attachments> attachmentDataList) throws Exception {
@@ -170,13 +171,15 @@ public class FastConverJNI {
     }
 
     private static int convert(FastConverJNI convertJni, byte[] templateOFDData, byte[] inData, List<MetaData> metaDataList, List<Attachments> attachmentDataList, int i) throws IOException {
+        //byte[] copyTemplateOFDData = Arrays.copyOf(templateOFDData, templateOFDData.length);
         byte[] resultData = convertJni.convert("001", templateOFDData, inData, metaDataList, attachmentDataList, 10);
-        if (1 == 1 && resultData != null) {
-            OutputStream resultOutStream = new FileOutputStream("D:\\gy\\c++\\projects\\template-ofd-converter\\data\\template-ofd-converter\\template4\\" + i + ".ofd");
+        if (1 == 2 && resultData != null) {
+            OutputStream resultOutStream = new FileOutputStream("D:\\gy\\c++\\projects\\template-ofd-converter\\data\\template-ofd-converter\\template12\\" + i + ".ofd");
             resultOutStream.write(resultData);
             resultOutStream.close();
             System.out.println("write result ofd successfully " + i + ".ofd");
         }
+        //System.out.println("write result ofd successfully " + i + ".ofd");
         return 0;
     }
 
